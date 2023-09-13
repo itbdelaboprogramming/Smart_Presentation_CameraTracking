cc134b03e78f6d83040398ba6fff76520ae628ce
Raihan Rafif <raihanrafif200095@gmail.com>
2023-09-13 12:02:04 +0700

    2 hands detection

--- learning-mediapipe/python/index.py
+++ learning-mediapipe/python/index.py
@@ -5,12 +5,13 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-
 mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
 
 model_path = './Smart_Presentation_CameraTracking/learning-mediapipe/python/gesture_recognizer.task'
+
+count = 0
 
 # Function to measure hand distance
 
@@ -47,10 +48,7 @@ def cancel_close_timeout():
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-        model_complexity=0,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
+with mp_hands.Hands(max_num_hands=2, model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -68,29 +66,68 @@ with mp_hands.Hands(
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
-            print("tttt", results.multi_hand_landmarks)
-            for hand_landmarks in results.multi_hand_landmarks:
-                firstHand = results.multi_hand_landmarks[0]
-                secondHand = results.multi_hand_landmarks[9]
+            # if count == 0 :
+            #     print("2 hands : ",results.multi_hand_landmarks)
+            #     count = count + 1
+            if len(results.multi_hand_landmarks) == 2:
+                first_hand = results.multi_hand_landmarks[0].landmark
+                for idx in first_hand:
+                    print(idx.x, idx.y, idx.z)
+                    x, y, z = idx.x, idx.y, idx.z
+                    # Tentukan posisi dan font untuk teks
+                    position = (
+                        int(x * image.shape[1]), int(y * image.shape[0]))
 
-                handsClose = checkHandDistance(firstHand, secondHand)
+                    # Warna bulatan (merah dalam format BGR)
+                    color = (0, 0, 255)
 
-                if handsClose and not isHandsClose:
-                    onHandsClose()
-                    isHandsClose = true
-                    setCloseTimeout()
+                    # Ukuran bulatan
+                    radius = 5
 
-                elif not handsClose and isHandsClose:
-                    cancelCloseTimeout()
-                    onHandsApart()
-                    isHandsClose = false
+                    # Menggambar bulatan pada posisi yang sesuai
+                    cv2.circle(image, position, radius, color, -1)
 
-                mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
+                    count = count + 1
+
+                second_hand = results.multi_hand_landmarks[1].landmark
+                for idx in second_hand:
+                    print(idx.x, idx.y, idx.z)
+                    x, y, z = idx.x, idx.y, idx.z
+                    # Tentukan posisi dan font untuk teks
+                    position = (
+                        int(x * image.shape[1]), int(y * image.shape[0]))
+
+                    # Warna bulatan (merah dalam format BGR)
+                    color = (0, 0, 255)
+
+                    # Ukuran bulatan
+                    radius = 5
+
+                    # Menggambar bulatan pada posisi yang sesuai
+                    cv2.circle(image, position, radius, color, -1)
+
+                    count = count + 1
+
+            # for hand_landmarks in results.multi_hand_landmarks:
+
+            # #     # handsClose = checkHandDistance(firstHand, secondHand)
+
+            # #     # if handsClose and not isHandsClose:
+            # #     #     onHandsClose()
+            # #     #     isHandsClose = true
+            # #     #     setCloseTimeout()
+
+            # #     # elif not handsClose and isHandsClose:
+            # #     #     cancelCloseTimeout()
+            # #     #     onHandsApart()
+            # #     #     isHandsClose = false
+
+            #     mp_drawing.draw_landmarks(
+            #         image,
+            #         hand_landmarks,
+            #         mp_hands.HAND_CONNECTIONS,
+            #         mp_drawing_styles.get_default_hand_landmarks_style(),
+            #         mp_drawing_styles.get_default_hand_connections_style())
 
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
