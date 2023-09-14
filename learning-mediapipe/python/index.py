fe5b178fc4812ea3eec365b6924bb5303de128ad
Raihan Rafif <raihanrafif200095@gmail.com>
2023-09-14 19:15:05 +0700

    repairing zooming algorithm

--- learning-mediapipe/python/index.py
+++ learning-mediapipe/python/index.py
@@ -2,9 +2,11 @@ from math import dist
 import mediapipe as mp
 import cv2
 import numpy as np
+import time
+import pyautogui
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-
 mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
@@ -12,38 +14,27 @@ cap = cv2.VideoCapture(0)
 model_path = './Smart_Presentation_CameraTracking/learning-mediapipe/python/gesture_recognizer.task'
 
 count = 0
+is_hands_close = False
+close_timeout = None
 
 # Function to measure hand distance
 
 
 def check_hand_distance(first_hand, second_hand):
-    thumb_distance = abs(first_hand[4]['x'] - second_hand[4]['x'])
+    thumb_distance = abs(first_hand[4].x - second_hand[4].x)
     return thumb_distance < 0.15
 
 
-def on_hands_close():
-    print("Tangan berdekatan, menjalankan fungsi...")
+# Zooming Function and Variable---------------------------------------------
+zoom_out_interval = None
+zoom_int_interval = None
+# ---------------------------------------------------------------
 
+def hands_apart(first_hand, second_hand):
+    thumb_distance = abs(first_hand[4].x - second_hand[4].x)
+    return thumb_distance > 0.15
 
-def on_hands_apart():
-    print("Tangan berjauhan, menjalankan fungsi...")
-
-
-def set_close_timeout():
-    global close_timeout, is_hands_close
-    close_timeout = None  # Inisialisasi timeout
-    # Waktu timeout dalam detik (1 detik dalam contoh ini)
-    close_timeout = time.time() + 1
-    is_hands_close = True
-
-
-def cancel_close_timeout():
-    global close_timeout, is_hands_close
-    if close_timeout:
-        if time.time() < close_timeout:
-            on_hands_close()  # Jika timeout belum tercapai, tangan tetap berdekatan
-        close_timeout = None
-        is_hands_close = False
+# ---------------------------------------------------------------
 
 
 # For webcam input:
@@ -66,68 +57,58 @@ with mp_hands.Hands(max_num_hands=2, model_complexity=0, min_detection_confidenc
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
-            # if count == 0 :
-            #     print("2 hands : ",results.multi_hand_landmarks)
-            #     count = count + 1
+            # only detect 2 hands
             if len(results.multi_hand_landmarks) == 2:
+
+                # first hand
                 first_hand = results.multi_hand_landmarks[0].landmark
+                # get all of coordinate in first hand
                 for idx in first_hand:
-                    print(idx.x, idx.y, idx.z)
-                    x, y, z = idx.x, idx.y, idx.z
-                    # Tentukan posisi dan font untuk teks
+
+                    # print(idx.x, idx.y, idx.z)
+                    x1, y1, z1 = idx.x, idx.y, idx.z
+
+                    # create mark in coordinate
                     position = (
-                        int(x * image.shape[1]), int(y * image.shape[0]))
-
-                    # Warna bulatan (merah dalam format BGR)
+                        int(x1 * image.shape[1]), int(y1 * image.shape[0]))
                     color = (0, 0, 255)
-
-                    # Ukuran bulatan
+                    # Mark radius
                     radius = 5
-
-                    # Menggambar bulatan pada posisi yang sesuai
                     cv2.circle(image, position, radius, color, -1)
 
-                    count = count + 1
-
+                # second hand
                 second_hand = results.multi_hand_landmarks[1].landmark
                 for idx in second_hand:
-                    print(idx.x, idx.y, idx.z)
-                    x, y, z = idx.x, idx.y, idx.z
-                    # Tentukan posisi dan font untuk teks
+                    x2, y2, z2 = idx.x, idx.y, idx.z
+
                     position = (
-                        int(x * image.shape[1]), int(y * image.shape[0]))
+                        int(x2 * image.shape[1]), int(y2 * image.shape[0]))
 
-                    # Warna bulatan (merah dalam format BGR)
                     color = (0, 0, 255)
-
-                    # Ukuran bulatan
                     radius = 5
 
-                    # Menggambar bulatan pada posisi yang sesuai
                     cv2.circle(image, position, radius, color, -1)
 
-                    count = count + 1
+                hands_close = check_hand_distance(first_hand, second_hand)
 
-            # for hand_landmarks in results.multi_hand_landmarks:
+                if hands_close:
+                    zoom_in_interval = time.time()+1
+                elif zoom_in_interval != None and time.time() < zoom_in_interval:
+                    if not hands_close:
+                        pyautogui.hotkey('ctrl', '+')
+                    zoom_in_interval = None
 
-            # #     # handsClose = checkHandDistance(firstHand, secondHand)
+                #
+                if not hands_close:
+                    zoom_out_interval = time.time()+1
+                elif zoom_out_interval != None and time.time() < zoom_out_interval:
+                    if hands_close:
+                        pyautogui.hotkey('ctrl', '-')
+                    zoom_out_interval = None
 
-            # #     # if handsClose and not isHandsClose:
-            # #     #     onHandsClose()
-            # #     #     isHandsClose = true
-            # #     #     setCloseTimeout()
-
-            # #     # elif not handsClose and isHandsClose:
-            # #     #     cancelCloseTimeout()
-            # #     #     onHandsApart()
-            # #     #     isHandsClose = false
-
-            #     mp_drawing.draw_landmarks(
-            #         image,
-            #         hand_landmarks,
-            #         mp_hands.HAND_CONNECTIONS,
-            #         mp_drawing_styles.get_default_hand_landmarks_style(),
-            #         mp_drawing_styles.get_default_hand_connections_style())
+            # 1 hand detection
+            # if len(results.multi_hand_landmarks) == 1:
+            #     print("tess")
 
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
